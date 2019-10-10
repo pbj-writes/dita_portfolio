d5b3a4509587a4c66316dd2e9510bc66209a437f
Philip Bolton Jr <Philip@PBJs-MacBook.local>
2019-10-09 21:10:51 -0700

    adding resume

---
+++ pbjs_resume/_wh/toc.js
@@ -0,0 +1,8 @@
+var whStorageId = "-bu6ulgi9vl2a";
+var whTOC = [
+["\n         <span class=\"webhelp-toc-section1-entry\">1. Summary</span>\n      ","Summary.html#Summary",null],
+["\n         <span class=\"webhelp-toc-section1-entry\">2. Experience</span>\n      ","Experience.html#Experience",null],
+["\n         <span class=\"webhelp-toc-section1-entry\">3. Education</span>\n      ","Education.html#Education",null],
+["\n         <span class=\"webhelp-toc-section1-entry\">4. Technical Skills</span>\n      ","technical_skills.html#technical_skills",null]
+];
+whWriteTOC(false);
