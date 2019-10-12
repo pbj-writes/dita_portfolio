6843a5902a913d01aeaf8af4581d74d471931407
Philip Bolton Jr <Philip@PBJs-MacBook.local>
2019-10-12 15:11:47 -0700

    new how to build this website files

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-define(function() {var keywords=[{w:"FICO",p:["p0"]},{w:"Bechtel",p:["p1"]},{w:"Marine",p:["p1"]},{w:"Propulsion",p:["p1"]},{w:"Corporation",p:["p1"]},{w:"(BMPC)",p:["p1"]},{w:"Contact",p:["p2"]},{w:"Education",p:["p3"]},{w:"Experience",p:["p4"]},{w:"Florida",p:["p5"]},{w:"International",p:["p5"]},{w:"University",p:["p5","p11","p12"]},{w:"(FIU)",p:["p5"]},{w:"Marathon",p:["p6"]},{w:"TS",p:["p6"]},{w:"Portfolio",p:["p7","p9"]},{w:"Resume",p:["p8","p9"]},{w:"About",p:["p9"]},{w:"this",p:["p9"]},{w:"and",p:["p9"]},{w:"Summary",p:["p10"]},{w:"State",p:["p11"]},{w:"of",p:["p11"]},{w:"New",p:["p11"]},{w:"York",p:["p11"]},{w:"College",p:["p11"]},{w:"at",p:["p11"]},{w:"Cortland",p:["p11"]},{w:"Syracuse",p:["p12"]},{w:"Technical",p:["p13"]},{w:"Skills",p:["p13"]},{w:"Word",p:["p14"]},{w:"Templates",p:["p14"]}];
+define(function() {var keywords=[{w:"FICO",p:["p0"]},{w:"Bechtel",p:["p1"]},{w:"Marine",p:["p1"]},{w:"Propulsion",p:["p1"]},{w:"Corporation",p:["p1"]},{w:"(BMPC)",p:["p1"]},{w:"Contact",p:["p2"]},{w:"Education",p:["p3"]},{w:"Experience",p:["p4"]},{w:"Florida",p:["p5"]},{w:"International",p:["p5"]},{w:"University",p:["p5","p13","p14"]},{w:"(FIU)",p:["p5"]},{w:"How-to...",p:["p6"]},{w:"...use",p:["p7"]},{w:"this",p:["p7","p11"]},{w:"template",p:["p7"]},{w:"Marathon",p:["p8"]},{w:"TS",p:["p8"]},{w:"Portfolio",p:["p9","p11"]},{w:"Resume",p:["p10","p11"]},{w:"About",p:["p11"]},{w:"and",p:["p11"]},{w:"Summary",p:["p12"]},{w:"State",p:["p13"]},{w:"of",p:["p13"]},{w:"New",p:["p13"]},{w:"York",p:["p13"]},{w:"College",p:["p13"]},{w:"at",p:["p13"]},{w:"Cortland",p:["p13"]},{w:"Syracuse",p:["p14"]},{w:"Technical",p:["p15"]},{w:"Skills",p:["p15"]},{w:"Video",p:["p16"]},{w:"Instructions",p:["p16"]},{w:"Word",p:["p17"]},{w:"Templates",p:["p17"]}];
 var ph={};
 ph["p0"]=[0];
 ph["p1"]=[1, 2, 3, 4, 5];
@@ -6,15 +6,18 @@ ph["p2"]=[6];
 ph["p3"]=[7];
 ph["p4"]=[8];
 ph["p5"]=[9, 10, 11, 12];
-ph["p6"]=[13, 14];
-ph["p7"]=[15];
-ph["p8"]=[16];
-ph["p9"]=[17, 18, 16, 19, 15];
+ph["p6"]=[13];
+ph["p7"]=[14, 15, 16];
+ph["p8"]=[17, 18];
+ph["p9"]=[19];
 ph["p10"]=[20];
-ph["p12"]=[28, 11];
-ph["p11"]=[21, 11, 22, 23, 24, 25, 26, 27];
-ph["p14"]=[31, 32];
-ph["p13"]=[29, 30];
+ph["p12"]=[23];
+ph["p11"]=[21, 15, 20, 22, 19];
+ph["p14"]=[31, 11];
+ph["p13"]=[24, 11, 25, 26, 27, 28, 29, 30];
+ph["p16"]=[34, 35];
+ph["p15"]=[32, 33];
+ph["p17"]=[36, 37];
      return {
          keywords: keywords,
          ph: ph
--- out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {8:-1,6:4,2:8,9:-1,7:-1,1:4,14:7,11:3,5:4,12:3,4:8,0:4,3:8,10:8,13:8};
+return {14:3,7:6,11:-1,4:10,1:4,3:10,5:4,16:9,2:10,0:4,12:10,15:10,8:4,10:-1,6:9,13:3,17:9,9:-1};
 });
